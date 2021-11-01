38d9f07a7ee792b4b84f272d2154df740654e724
OO-b <baeiiyy@gmail.com>
2021-11-01 21:49:55 +0900

    add room number & user id

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,8 @@ def index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
+    id = "111@naver.com"
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'id': id
     })
